6ee4701abde67621112910f613caa4c88f682049
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-10-02 21:47:47 -0700

    fetchPosts method functionality implemented using redux

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
 
 class Posts extends Component {
-    
-    constructor(props){
-      super(props);
-      this.state = {
-          posts: [],
-          post: {}
-      }
-    }
-    
-    componentWillMount(){
-        fetch('https://jsonplaceholder.typicode.com/posts')
-          .then(res => res.json())
-          .then(data => this.setState({posts: data}));    
+
+    componentWillMount() {
+     this.props.fetchPosts();    
     }
     
     render (){
-        
-        const postItems = this.state.posts.map(post => (
+        const postItems = this.props.posts.map(post => (
           <div key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.body}</p>
@@ -31,7 +23,16 @@ class Posts extends Component {
             { postItems }
         </div>
         );
-    }
+    }   
 }
 
-export default Posts;
+Posts.protoType = {
+    fetchPosts: PropTypes.func.isRequired,
+    post: PropTypes.array.isRequired
+}
+
+const mapStateToProps = state => ({
+    posts: state.posts.items
+});
+
+export default connect(mapStateToProps, { fetchPosts } )(Posts);
